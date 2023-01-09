91f839038ed380f4c3e98c5102f0d594c3c4690d
Sultonbek Tulanov <116726923+SultonbekPGMR@users.noreply.github.com>
2023-01-09 12:57:01 +0500

    Initial commit

---
+++ app/src/main/java/com/codialstudent/phoneapp/Data.kt
@@ -0,0 +1,7 @@
+package com.codialstudent.phoneapp
+
+data class Data(
+    val name: String,
+    val products: String,
+    val order: String,
+):java.io.Serializable
---
+++ app/src/main/java/com/codialstudent/phoneapp/ShowInfoActivity.kt
@@ -0,0 +1,22 @@
+package com.codialstudent.phoneapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codialstudent.foodapp.databinding.ActivityShowInfoBinding
+
+class ShowInfoActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityShowInfoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityShowInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val list: Data = intent.getSerializableExtra("list") as Data
+        supportActionBar?.title = list.name.uppercase()
+        binding.tvProduct.text = list.products
+        binding.tvOrder.text = list.order
+
+
+
+    }
+}
